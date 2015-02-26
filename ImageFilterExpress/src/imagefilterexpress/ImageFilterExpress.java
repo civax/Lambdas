865bab4211b309b323c47b59ae47c2ebdf5ec140
carcasti <carcasti@CARCASTI-LAP.oradev.oraclecorp.com>
2015-02-26 14:37:49 -0600

    Esqueleto de la aplicacion

---
+++ ImageFilterExpress/src/imagefilterexpress/ImageFilterExpress.java
@@ -0,0 +1,14 @@
+//esta clase tiene propositos de testing
+
+package imagefilterexpress;
+
+public class ImageFilterExpress {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
