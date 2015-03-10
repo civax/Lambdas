b3e8a3e71aadfff2890b01cf2023350709cc2bea
carcasti <carcasti@CARCASTI-LAP.oradev.oraclecorp.com>
2015-03-09 22:50:58 -0600

    agregue metodo main a los servidores para poderlos arrancar de manera independiente

--- ImageFilterExpress/src/net/client/ui/ClientUI.java
+++ ImageFilterExpress/src/net/client/ui/ClientUI.java
@@ -1,5 +1,4 @@
-/*
- * Copyright (C) 2015 carcasti
+ /* Copyright (C) 2015 carcasti
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
